86c25981859b34e1b165be0b61b8f11bf88e723a
Nilton da Silva Junior <tonsti@gmail.com>
2020-11-20 21:09:21 +0100

    Add Lesson, Task and Content entities and seeding

--- backend/src/main/java/com/devsuperior/dslearn/entities/Enrollment.java
+++ backend/src/main/java/com/devsuperior/dslearn/entities/Enrollment.java
@@ -1,17 +1,23 @@
 package com.devsuperior.dslearn.entities;
 
+import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import com.devsuperior.dslearn.entities.pk.EnrollmentPK;
 
 @Entity
 @Table(name = "tb_enrollment")
-public class Enrollment {
+public class Enrollment implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	@EmbeddedId
 	private EnrollmentPK id = new EnrollmentPK();	
@@ -23,6 +29,9 @@ public class Enrollment {
 	private Instant refundMoment;
 	private boolean available;
 	private boolean onlyUpdate;
+	
+	@ManyToMany(mappedBy = "enrollmentsDone")	
+	private Set<Lesson> lessonsDone = new HashSet<>();
 	
 	public Enrollment() {
 		
